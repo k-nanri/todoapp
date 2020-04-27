f43fa47aa3f5be89fcc870f0c66a053dd93b9356
k-nanri <kotaro.nanri15@gmail.com>
2020-04-27 23:21:58 +0900

    テキストフィールドとリストの操作をクラス分け

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,56 @@
+class TodoItem {
+    private todo:string = "";
 
-const button = document.getElementById("regist");
+    constructor() {
 
-if (button) {
-    button.onclick = ():void => {
-        
-        // valueは、HTMLInputElementが実装しているため型アサーションする
-        const textbox:HTMLInputElement = document.getElementById("todo") as HTMLInputElement
-        let todo:string = "";
-        if (textbox) {
-            // テキストボックスから値を取得
-            todo = textbox.value;
+        const todoTextElement:HTMLInputElement | null = document.getElementById("todo") as HTMLInputElement;
+        if (!todoTextElement) {
+            throw Error("Not Found HTMLElement");
         }
+
+        this.todo = todoTextElement.value;
+    }
+
+    public getTodo(): string {
+        return this.todo;
+    }
+}
+
+class TodoList {
+
+    private todolist:HTMLElement;
+
+    constructor() {
+        const element:HTMLElement | null = document.getElementById("todoitems");
+        if (element === null) {
+            throw Error("Not Found HTMLElement.");
+        }
+        this.todolist = element;
+    }
+
+    public pushTodo(todo:string) {
         
-        // todoのリストにテキストボックスから取得したtodoを書き込む
-        const unsortlist = document.getElementById("todoitems");
         const list = document.createElement("li");
         const textnode = document.createTextNode(todo);
         list.appendChild(textnode);
-        if (unsortlist) {
-            unsortlist.appendChild(list);
-        }
+        this.todolist.appendChild(list);
+    }
+
+}
+
+export function init() {
+    const button = document.getElementById("regist");
+    if (button) {
+        button.onclick = () => {
+            try {
+                const todo:TodoItem = new TodoItem();
+                const todolist:TodoList = new TodoList();
+                todolist.pushTodo(todo.getTodo());
+            } catch (e) {
+
+            }
+        };
     }
 }
+
+init();
